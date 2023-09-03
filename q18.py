37315fb091074e96e95e96838c8b248140136e03
Aditya Tripathi <addykoder@gmail.com>
2023-09-03 21:16:39 +0530

    added q18

--- q18.py
+++ q18.py
@@ -1,9 +1,12 @@
 import csv
 
-
+#phone.txt must exist
 file = open('phone.txt', 'r')
 reader = csv.reader(file)
 
+for consumer in reader:
+    if consumer[0][0] in 'dD':
+        print(f'{consumer[0]} {consumer[1]}')
 
 
 file.close()
